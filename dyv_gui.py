c39449951c78a4bc160fe1c6d34d63e6bfdfc284
Panos-Jr <panos.etosjr@gmail.com>
2023-09-04 10:43:39 +0100

    clean code

--- dyv_gui.py
+++ dyv_gui.py
@@ -10,46 +10,20 @@ def is_valid_youtube_url(url):
     youtube_regex = r'^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$'
     return re.match(youtube_regex, url)
 
-# def progress_hook(d):
-#     if d['status'] == 'downloading':
-#         if 'fragment_index' in d:
-#             status = f'Downloading {d["fragment_index"]}/{d["fragment_count"]} fragments'
-#         else:
-#             status = 'Downloading'
-#     elif d['status'] == 'finished':
-#         status = 'Download completed'
-#     else:
-#         status = d['status']
-#     downloaded = d.get('downloaded_bytes', 0)
-#     total = d.get('total_bytes', 0)
-#     if total > 0:
-#         progress = math.floor(int((downloaded / total) * 100))
-#         progress_bar['value'] = progress
-#     status_label.config(text=status)
-
 def progress_hook(d):
     global status
     status = None
     if d['status'] == 'downloading':
         if 'fragment_index' in d and d['fragment_index'] > 0:
-                progress = math.floor(int(d["fragment_index"])/int(d["fragment_count"])*100)
-                status = f'Downloading {d["fragment_index"]}/{d["fragment_count"]} ({progress}%)'
-                progress_bar_var.set(progress)
+            progress = math.floor(int(d["fragment_index"])/int(d["fragment_count"])*100)
+            status = f'Downloading {d["fragment_index"]}/{d["fragment_count"]} ({progress}%)'
+            progress_bar_var.set(progress)
     elif d['status'] == 'finished':
         status = 'Download completed'
     else:
         status = d['status']
     status_label.config(text=status)
-
     root.update()
-
-
-
-def download_best_audio(url, ydl):
-    ydl.download([url])
-
-def download_best_combined(url, ydl):
-    ydl.download([url])
 
 def find_file(download_folder, file_name):
     files_in_folder = os.listdir(download_folder)
@@ -57,6 +31,52 @@ def find_file(download_folder, file_name):
         name, ext = os.path.splitext(file_ident)
         if file_name == name:
             return file_ident
+
+def download_media(url, selection):
+    try:
+        download_status_label.config(text=f'Starting download...')
+        info = yt_dlp.YoutubeDL().extract_info(url, download=False)
+        title = info['title']
+        desktop = os.path.join(os.environ['USERPROFILE'], 'Desktop')
+        download_folder = os.path.join(desktop, 'downloads')
+        file_name = find_file(download_folder, title)
+        
+        if file_name:
+            enable_download_button()
+            download_status_label.destroy()
+            messagebox.showinfo("File already exists", f'You\'ve already downloaded this.')
+            return
+        
+        progress_bar.grid()
+        
+        ydl_opts = {
+            'format': 'bestaudio/best' if selection == 'a' else 'bestvideo+bestaudio/best',
+            'progress_hooks': [progress_hook],
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }] if selection == 'a' else [],
+            'outtmpl': os.path.join(download_folder, f'%(title)s.%(ext)s'),
+            'quiet': True,
+            'no_warnings': True
+        }
+        
+        ydl = yt_dlp.YoutubeDL(ydl_opts)
+        download_status_label.destroy()
+        ydl.download([url])
+        
+        if selection == 'a':
+            messagebox.showinfo("Success", f'Audio saved in \'downloads\'')
+        else:
+            messagebox.showinfo("Success", f'Video saved in \'downloads\'')
+        
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {e}")
+    finally:
+        progress_bar.grid_remove()
+        status_label.destroy()
+        enable_download_button()
 
 def disable_download_button():
     download_button.config(state="disabled")
@@ -71,69 +91,13 @@ def download_video():
     if not url:
         messagebox.showerror("Error", "Please enter a YouTube URL.")
         return
-
+    
     if not is_valid_youtube_url(url):
         messagebox.showerror("Error", "Invalid YouTube URL.")
         return
-
-    def download():
-        try:
-            download_status_label.config(text=f'Starting download...')
-            info = yt_dlp.YoutubeDL().extract_info(url, download=False)
-            title = info['title']
-            desktop = os.path.join(os.environ['USERPROFILE'], 'Desktop')
-            download_folder = os.path.join(desktop, 'downloads')
-            file_name = find_file(download_folder, title)
-            if file_name:
-                enable_download_button()
-                download_status_label.destroy()
-                messagebox.showinfo("File already exists", f'You\'ve already downloaded this.')
-                return
-            progress_bar.grid()
-
-            if selection == 'a':
-                ydl_opts = {
-                    'format': 'bestaudio/best',
-                    'progress_hooks': [progress_hook],
-                    'postprocessors': [{
-                        'key': 'FFmpegExtractAudio',
-                        'preferredcodec': 'mp3',
-                        'preferredquality': '192',
-                    }],
-                    'outtmpl': os.path.join(download_folder, f'%(title)s.%(ext)s'),
-                    'quiet': True,
-                    'no_warnings': True
-                }
-
-                ydl = yt_dlp.YoutubeDL(ydl_opts)
-                download_status_label.destroy()
-                download_best_audio(url, ydl)
-                messagebox.showinfo("Success", f'Audio saved in \'downloads\'')
-            else:
-                ydl_opts = {
-                    'format': 'bestvideo+bestaudio/best',
-                    'progress_hooks': [progress_hook],
-                    'outtmpl': os.path.join(download_folder, f'%(title)s.%(ext)s'),
-                    'quiet': True,
-                    'no_warnings': True
-                }
-
-                ydl = yt_dlp.YoutubeDL(ydl_opts)
-                download_status_label.destroy()
-                download_best_combined(url, ydl)
-                messagebox.showinfo("Success", f'Video saved in \'downloads\'')
-
-
-        except Exception as e:
-            messagebox.showerror("Error", f"An error occurred: {e}")
-        finally:
-            progress_bar.grid_remove()
-            status_label.destroy()
-        enable_download_button()
-
+    
     disable_download_button()
-
-    download_thread = threading.Thread(target=download)
+    download_thread = threading.Thread(target=lambda: download_media(url, selection))
     download_thread.start()
 
 root = Tk()
@@ -171,8 +135,5 @@ download_status_label.grid(row=0, column=0, columnspan=2, pady=(10,5))
 
 progress_bar_var = DoubleVar()
 progress_bar = ttk.Progressbar(status_progress_frame, orient=HORIZONTAL, length=300, mode="determinate", variable=progress_bar_var)
-progress_bar.grid(row=2, column=0, columnspan=2, pady=(10, 5))
-progress_bar.grid_remove()
-
 
 root.mainloop()
