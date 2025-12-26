e9583f7c86f6f8fe6eec5c7a1cc3a0aca4a80c5d
Panos Sakketos <panos.etosjr@gmail.com>
2025-12-26 01:06:19 +0000

    remove unnecessary step in filename sanitation

--- utils.py
+++ utils.py
@@ -4,6 +4,5 @@ ILLEGAL_CHARACTERS = r'[\\/:*?"<>|{}$!\'"&%`@+=,;]'
 
 def sanitize_filename(filename):
     sanitized = re.sub(ILLEGAL_CHARACTERS, '', filename)
-    sanitized = re.sub(r'[^\x00-\x7F]+', '', sanitized)
     sanitized = re.sub(r'_+', '_', sanitized).strip('_')
     return sanitized
